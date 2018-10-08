02679d64418746982c70cf379230c8b67ff4f451
Jayant <jayantp@sahajsoft.com>
2018-10-08 16:46:42 +0530

    ENH: Adding functional UI with lda model results

--- python/app/app.py
+++ python/app/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+import requests
+import urllib
+import re
+from bs4 import BeautifulSoup
+
 
 import os
 import sys
@@ -9,15 +14,44 @@ from scripts.lda_model_loader import LdaModel
 
 app = Flask(__name__)
 MODEL = LdaModel()
+API_URI = 'https://storyweaver.org.in/api/v1/'
+
 
 @app.route('/', methods=['GET'])
 @app.route('/<query>')
 def index():
-    print(request.args.get('query'))
-    print(MODEL.predict('Elephant'))
+    if 'query' in request.args:
+        q = request.args['query']
+        story_path = urllib.parse.urlparse(q).path
+        api_uri_path = get_api_path(story_path)
+        text = get_pages_text(requests.get(api_uri_path).json())
+        tags = [{'model': 'LDA model',
+                 'tags': ', '.join(MODEL.predict(text))}
+                ]
+        return render_template('index.html', tags=tags)
     return render_template('index.html')
 
 
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
+def get_pages_text(resp):
+    '''Parse the request from the storyweaver api and get text of the story book
+    '''
+    pages = resp['data']['pages']
+    texts = []
+    for page in pages:
+        cleantext = BeautifulSoup(page['html'], "lxml").text.replace('\n', ' ')
+        texts.append(cleantext)
+    return ' '.join(texts)
+
+
+def get_api_path(story_path):
+    api_path = API_URI + story_path + '/read'
+    return api_path
 
 
 if __name__ == '__main__':
